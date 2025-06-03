45148c0ab56fafb743f29bf76c03db9988235baf
Gabriel-Lippi-Costa <gabriellippidacosta@gmail.com>
2025-06-03 16:37:19 -0300

    Classe Pessoa tem construtor com a lombok

--- SistemaAcademico2/src/main/java/com/mycompany/sistemaacademico2/Pessoa.java
+++ SistemaAcademico2/src/main/java/com/mycompany/sistemaacademico2/Pessoa.java
@@ -1,11 +1,13 @@
 
 package com.mycompany.sistemaacademico2;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Pessoa {
     
     private int idUsuario;
@@ -13,9 +15,4 @@ public class Pessoa {
     private String fone;
     private String email;
     
-    
-    public void cadastrar() {
-        String sql = "insert into tb_pessoa(nome, fone, email) values (?, ?, ?)";
-        
-    }
 }
