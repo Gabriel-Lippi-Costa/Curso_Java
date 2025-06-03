f8b27a931ff4c5e28d1a7140f8617abd1969c36c
Gabriel-Lippi-Costa <gabriellippidacosta@gmail.com>
2025-06-03 16:23:32 -0300

    Classe Pessoa está pronta

--- SistemaAcademico2/src/main/java/com/mycompany/sistemaacademico2/Pessoa.java
+++ SistemaAcademico2/src/main/java/com/mycompany/sistemaacademico2/Pessoa.java
@@ -1,6 +1,21 @@
 
 package com.mycompany.sistemaacademico2;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Pessoa {
     
+    private int idUsuario;
+    private String nome;
+    private String fone;
+    private String email;
+    
+    
+    public void cadastrar() {
+        String sql = "insert into tb_pessoa(nome, fone, email) values (?, ?, ?)";
+        
+    }
 }
